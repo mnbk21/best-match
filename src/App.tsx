adae135aa4d54ebd198050e7ad68e4dd0f284fd5
mnbk21 <mnbk21@gmail.com>
2024-06-24 22:58:18 +0900

    ログイン機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Register from "./pages/Register"
@@ -12,6 +12,8 @@ const App = () => {
     password: "",
   })
 
+  const [loggedIn, setLoggedIn] = useState(false)
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     
     setUser({
@@ -21,17 +23,29 @@ const App = () => {
 
   }
 
+  console.log(loggedIn)
+
   const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     localStorage.setItem("user-data" , JSON.stringify(user))
+    setLoggedIn(true)
   }
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user-data") || "{}")
+    userData && setLoggedIn(true)
+  }, [])
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<h1>ページがありません</h1>}/>
         <Route path="/register" element={<Register handleChange={handleChange} handleRegister={handleRegister}/>}/>
-        <Route path="/search" element={<Search/>}/>
+        { loggedIn &&
+          <Route path="/search" element={<Search/>}/>
+        }
+          
       </Routes>
     </>
   )
