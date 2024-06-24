54a917f2290078b197757d8a0c32bc897dfa4488
mnbk21 <mnbk21@gmail.com>
2024-06-24 22:28:06 +0900

    Local Strage設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 
+import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Register from "./pages/Register"
 import Search from "./pages/Search"
 
 const App = () => {
+
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+    setUser({
+      ...user, // スプレッド構文
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+  const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    localStorage.setItem("user-data" , JSON.stringify(user))
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/register" element={<Register/>}/>
+        <Route path="/register" element={<Register handleChange={handleChange} handleRegister={handleRegister}/>}/>
         <Route path="/search" element={<Search/>}/>
       </Routes>
     </>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,19 @@
 
-const Register = () => {
+type RegisterProps = {
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleRegister: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const Register = ({handleChange, handleRegister}: RegisterProps) => {
   return (
-    <h2>登録ページ</h2>
+    <>
+      <h2>登録ページ</h2>
+      <form onSubmit={handleRegister}>
+        <input onChange={handleChange} type="text" name="name" placeholder="ユーザーネーム" required />
+        <input onChange={handleChange} type="text" name="password" placeholder="パスワード" required />
+        <button>登録</button>
+      </form>
+    </>
   )
 }
 
