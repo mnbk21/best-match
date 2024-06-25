b6f57eac7222dfc941a88fc97f0029d2c02b81dc
mnbk21 <mnbk21@gmail.com>
2024-06-25 23:21:40 +0900

    候補者表示

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,34 @@
 
+import { useState } from "react"
+import Button from "../components/Button"
+import List from "../components/List"
+
+export type CandidateProps = {
+  firstName: string;
+  email: string;
+  height: number;
+  // CandidateProps: string;
+}
+
 const Search = () => {
+
+  const [candidates, setCandidates] = useState<CandidateProps[]>([])
+
+  const getCandidatesData = () => {
+    fetch("https://dummyjson.com/users?limit=30")
+    .then(res => res.json())
+    .then(data => {
+      setCandidates(data.users)
+      console.log(data.users)
+    })
+  }
+
   return (
-    <h2>検索ページ</h2>
+    <div className="search">
+      <h2>検索ページ</h2>
+      <Button text="候補者を表示" handler={getCandidatesData} buttonColor="#2c7ce5" padding={[6, 40, 6, 40]}/>
+      <List candidates={candidates}/>
+    </div>
   )
 }
 
